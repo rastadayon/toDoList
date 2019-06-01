23d7e0da173c1c6b15815238da07aaa4e4144b8c
rastadayon <rasta.tadayon1378@gmail.com>
2019-06-01 19:56:43 +0430

    main body almost done

---
+++ toDoList.js
@@ -0,0 +1,25 @@
+$(document).ready(
+    function(){
+        $('.addIcon').click(
+            function(){
+                var toAdd = $('input[name=addItem]').val();
+                 $('ol').append('<li>' + toAdd + '</li>');
+            });
+       
+    //    $("input[name=ListItem]").keyup(function(event){
+    //       if(event.keyCode == 13){
+    //         $("#button").click();
+    //       }         
+    //   });
+      
+    //   $(document).on('dblclick','li', function(){
+    //     $(this).toggleClass('strike').fadeOut('slow');    
+    //   });
+      
+    //   $('input').focus(function() {
+    //     $(this).val('');
+    //   });
+      
+    //   $('ol').sortable();  
+      
+ });
